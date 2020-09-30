d76b41908559ba8719e4e35001222d0b409824d1
ku13shov <kuleshov000@gmail.com>
2020-09-30 23:19:54 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && array.length) {
+    return Math.min.apply(null, array);
+  } 
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array.length) {
+    return Math.max.apply(null, array);
+  } 
   return 0;
 }
 
 exports.avg = function avg (array) {
+  let res = 0;
+  if (Array.isArray(array) && array.length) {
+    for (let i = 0; i < array.length; i ++) {
+      res += array[i];
+      }
+      res /= array.length;
+      return res;
+  }
   return 0;
 }
